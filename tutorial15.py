bbf4513cfc75300daaedb6d82cac0adcf27901a4
Shakyo choudhury <94360466+Shakyo-choudhury@users.noreply.github.com>
2023-08-11 01:13:54 +0530

    Add files via upload

---
+++ tutorial15.py
@@ -0,0 +1,12 @@
+# join
+words = ['Hello', 'world']
+sentence = ' '.join(words)
+print(sentence)  # Output: "Hello world"
+
+message = "Python programming"
+print(message.find("gram"))  # Output: 10
+print(message.index("pro"))  # Output: 7
+
+
+sentence = "How much wood would a woodchuck chuck if a woodchuck could chuck wood?"
+print(sentence.count("wood"))  # Output: 2
